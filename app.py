7af9a15dcf8b34ce4c79bab8519c9f5e6d568291
OptoDevz <108997822+OptoDevz@users.noreply.github.com>
2022-08-21 15:47:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ title = "URL SHORTENER"
 choice = ["urlshortener", "exit", "troll"]
 reply = easygui.buttonbox(msg, title, choice)
 if reply == "urlshortener":
-    os.startfile("D:\\Downloads\\Datasets & yes\\proj\\urlshor\\urlshor.py")
+    file = easygui.enterbox("Enter your directory for urlshor.py: ", title)
+    os.startfile(file)
 elif reply == exit:
     os.system("Exiting..")
     os._exit
 elif reply == "troll":
-    easygui.msgbox("You got big time trolled")
+    easygui.msgbox("You got big time trolled", title)
